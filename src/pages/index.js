d7380413db935cebe9e0b3563200f180b916d6d4
James Hooper <jameshooperdesigns@gmail.com>
2020-03-01 14:13:17 -0700

    Add this is a test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi Manuel and James!</h1>
-    <p>This is our new React Tip</p>
+    <h2>This is a test.</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
