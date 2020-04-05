c11b464f83bbb70575ca419bec25bb119ac6359e
manoy96 <mfewcar@gmail.com>
2020-04-05 13:09:59 -0600

    placeholder icons spots

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
